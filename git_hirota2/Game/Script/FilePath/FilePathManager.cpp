1098b96cf0f35590b062549f549f84763f3c0e0a
horo <hirotaryuki126@gmail.com>
2025-06-08 10:31:38 +0900

    <ファイルパス>ファイルパスマネージャーの追加

---
+++ git_hirota2/Game/Script/FilePath/FilePathManager.cpp
@@ -0,0 +1,3 @@
+﻿#include "stdafx.h"
+#include "FilePathManager.h"
+FilePathManager* FilePathManager::m_instance = nullptr;
---
+++ git_hirota2/Game/Script/FilePath/FilePathManager.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+class FilePathManager
+{
+private:
+    FilePathManager() {};
+    ~FilePathManager() {};
+public:
+    static void CreateInstance()
+    {
+        //�x���B
+        K2_ASSERT(m_instance == nullptr, "FilePathManager instance is created yet.");
+        m_instance = new FilePathManager();
+
+    }
+
+    static FilePathManager* GetInstance() {
+        K2_ASSERT(m_instance != nullptr, "FilePathManager instance is not created yet. Call CreateInstance() first.");
+        return m_instance;
+    }
+
+    static void ReleaseInstance()
+    {
+        delete m_instance;
+        m_instance = nullptr;
+    }
+
+    static FilePathManager* m_instance;
+    std::unordered_map<std::string, std::string> m_paths;
+    std::string m_basePath;
+public:
+
+};
+
