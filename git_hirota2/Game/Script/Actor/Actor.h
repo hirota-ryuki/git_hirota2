50c4efa03ebf6a392cde5031beb7b4c28f75202d
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-09 20:06:12 +0900

    <アクター>基底クラスの追加

---
+++ git_hirota2/Game/Script/Actor/Actor.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+class Actor :  public IGameObject
+{
+public:
+    Actor();
+    ~Actor();
+    virtual bool Start() override;
+    virtual void Update() override;
+    virtual void Render(RenderContext& rc) override;
+protected:  //本クラスで作成した変数は、派生クラスで関数越しにアクセスせず、直接変数にアクセスしてもよい。
+    
+};
+
