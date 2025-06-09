ff39522cff58ee264d363521eb91cf36ad16df9b
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-09 20:05:47 +0900

    <プレイヤー>スクリプトの追加

---
+++ git_hirota2/Game/Script/Actor/Character/Player/Player.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdafx.h"
+#include "Player.h"
+#include "Actor/Character/Character.h"
+
+Player::Player()
+{
+
+}
+
+Player::~Player()
+{
+}
+
+bool Player::Start()
+{
+    return true;
+}
+
+void Player::Update()
+{
+}
+
+void Player::Render(RenderContext& rc)
+{
+}
---
+++ git_hirota2/Game/Script/Actor/Character/Player/Player.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Actor/Character/Character.h"
+
+class Player :
+    public Character
+{
+    Player();
+    ~Player();
+    bool Start() override;
+    void Update() override;
+    void Render(RenderContext& rc) override;
+};
+
