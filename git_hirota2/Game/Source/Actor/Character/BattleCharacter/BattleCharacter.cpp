74f8c4727ee209981a980b41829a71e2d4b0ee1d
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-11 19:32:32 +0900

    <バトルキャラクター>プレイアブルキャラクターの増加等の拡張性を上げるためプレイヤークラスから変更

---
+++ git_hirota2/Game/Source/Actor/Character/BattleCharacter/BattleCharacter.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include "BattleCharacter.h"
+
+namespace nsGame {
+    BattleCharacter::BattleCharacter()
+    {
+
+    }
+
+    BattleCharacter::~BattleCharacter()
+    {
+    }
+
+    bool BattleCharacter::Start()
+    {
+        return true;
+    }
+
+    void BattleCharacter::Update()
+    {
+    }
+
+    void BattleCharacter::Render(RenderContext& rc)
+    {
+    }
+}
---
+++ git_hirota2/Game/Source/Actor/Character/BattleCharacter/BattleCharacter.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Actor/Character/Character.h"
+namespace nsGame {
+    class BattleCharacter :
+        public Character
+    {
+        BattleCharacter();
+        ~BattleCharacter();
+        bool Start() override;
+        void Update() override;
+        void Render(RenderContext& rc) override;
+    };
+}
