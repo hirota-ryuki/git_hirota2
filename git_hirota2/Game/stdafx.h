fc24f6790adf7a8833a16bfeeebe23fe9c5d6fa9
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-13 18:28:24 +0900

    <リソースマネージャー>インクルードの追加

--- git_hirota2/Game/stdafx.h
+++ git_hirota2/Game/stdafx.h
@@ -1,6 +1,9 @@
-#pragma once
+﻿#pragma once
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "k2EnginePreCompile.h"
 using namespace nsK2EngineLow;
 using namespace nsK2Engine;
+
+//追加したいヘッダファイルをここに追加してください。
+#include "Resource/ResourceManager.h"
