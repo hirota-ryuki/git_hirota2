e6d2f3e176dd900299fc8cc0b0e6a02caad48dde
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-13 18:15:56 +0900

    <アクター>インデントの調整

--- git_hirota2/Game/Source/Actor/Actor.cpp
+++ git_hirota2/Game/Source/Actor/Actor.cpp
@@ -3,21 +3,21 @@
 #include "Source/Transform/Transform.h"
 #include <memory>
 
-namespace nsGame{
-//ヘッダで宣言した変数は、ヘッダで初期化するのではなく、コンストラクタで初期化すること。
-//ローカル環境の話になるが、コンパイル時間を減らすためである。
-//例：変数の初期値を変えた時、
-//ヘッダをインクルードしている全てのソースファイルが再コンパイルされてしまうため、
-//コンパイル時間がその分長くなってしまう。
-Actor::Actor()
-{
-    m_transform = std::make_shared<nsEngine::Transform>();
-}
+namespace nsGame {
+    //ヘッダで宣言した変数は、ヘッダで初期化するのではなく、コンストラクタで初期化すること。
+    //ローカル環境の話になるが、コンパイル時間を減らすためである。
+    //例：変数の初期値を変えた時、
+    //ヘッダをインクルードしている全てのソースファイルが再コンパイルされてしまうため、
+    //コンパイル時間がその分長くなってしまう。
+    Actor::Actor()
+    {
+        m_transform = std::make_shared<nsEngine::Transform>();
+    }
 
-Actor::~Actor()
-{
-    m_transform.reset();
-}
+    Actor::~Actor()
+    {
+        m_transform.reset();
+    }
 
     bool Actor::Start()
     {
