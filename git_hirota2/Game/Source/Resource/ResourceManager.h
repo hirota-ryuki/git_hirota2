e46458801fcf4127a7927624270fc2afeaeee3ac
Hitota Ryuki <hogehoge0125@gmail.com>
2025-06-13 18:26:56 +0900

    <リソースマネージャー>ネームスペースの追加

--- git_hirota2/Game/Source/Resource/ResourceManager.h
+++ git_hirota2/Game/Source/Resource/ResourceManager.h
@@ -1,46 +1,46 @@
 ﻿#pragma once
-class ResourceManager
-{
-private:
-    ResourceManager() {};
-    ~ResourceManager() {};
-public:
-    /// <summary>
-    /// シングルトンインスタンスを生成する。
-    /// </summary>
-    static void CreateInstance()
+namespace nsGame {
+    class ResourceManager
     {
-        //警告: インスタンスが既に生成されている場合は、例外を投げる。
-        K2_ASSERT(m_instance == nullptr, "ResourceManager instance is created yet.");
-        // インスタンスを生成する。
-        m_instance = new ResourceManager();
-    }
+    private:
+        ResourceManager() {};
+        ~ResourceManager() {};
+    public:
+        /// <summary>
+        /// シングルトンインスタンスを生成する。
+        /// </summary>
+        static void CreateInstance()
+        {
+            //警告: インスタンスが既に生成されている場合は、例外を投げる。
+            K2_ASSERT(m_instance == nullptr, "ResourceManager instance is created yet.");
+            // インスタンスを生成する。
+            m_instance = new ResourceManager();
+        }
 
-    /// <summary>
-    /// シングルトンインスタンスを取得する。
-    /// </summary>
-    static ResourceManager* GetInstance() {
-        //警告: インスタンスが生成されていない場合は、例外を投げる。
-        K2_ASSERT(m_instance != nullptr, "ResourceManager instance is not created yet. Call CreateInstance() first.");
-        // インスタンスを取得する。
-        return m_instance;
-    }
+        /// <summary>
+        /// シングルトンインスタンスを取得する。
+        /// </summary>
+        static ResourceManager* GetInstance() {
+            //警告: インスタンスが生成されていない場合は、例外を投げる。
+            K2_ASSERT(m_instance != nullptr, "ResourceManager instance is not created yet. Call CreateInstance() first.");
+            // インスタンスを取得する。
+            return m_instance;
+        }
 
-    /// <summary>
-    /// シングルトンインスタンスを解放する。
-    /// </summary>
-    static void ReleaseInstance()
-    {
-        // インスタンスを解放する。
-        delete m_instance;
-        m_instance = nullptr;
-    }
+        /// <summary>
+        /// シングルトンインスタンスを解放する。
+        /// </summary>
+        static void ReleaseInstance()
+        {
+            // インスタンスを解放する。
+            delete m_instance;
+            m_instance = nullptr;
+        }
 
-public:
-    static ResourceManager* m_instance; // シングルトンインスタンス本体。
-private:
-    std::unordered_map<std::string, std::string> m_paths;    // 複数のパス情報を管理する連想配列。
-    std::string                                  m_basePath; // 基準となるパス。
-};
-
-
+    public:
+        static ResourceManager* m_instance; // シングルトンインスタンス本体。
+    private:
+        std::unordered_map<std::string, std::string> m_paths;    // 複数のパス情報を管理する連想配列。
+        std::string                                  m_basePath; // 基準となるパス。
+    };
+}
