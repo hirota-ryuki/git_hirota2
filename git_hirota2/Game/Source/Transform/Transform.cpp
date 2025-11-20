2a4dada6555e8d5548a1041c8dd3f33d84ca85a8
Hitota Ryuki <hogehoge0125@gmail.com>
2025-11-20 20:16:37 +0900

    <トランスフォーム>新コーディングルールの適用とメンバ変数及び関数の追加(未完成)

--- git_hirota2/Game/Source/Transform/Transform.cpp
+++ git_hirota2/Game/Source/Transform/Transform.cpp
@@ -1,11 +1,33 @@
 ﻿#include "stdafx.h"
 #include "Transform.h"
-namespace nsEngine {
+
+
+namespace nsApp {
     Transform::Transform()
+        :m_localPosition(Vector3::Zero)
+        , m_localRotation(Quaternion::Identity)
+        , m_localScale(Vector3::One)
+        , m_position(Vector3::Zero)
+        , m_rotation(Quaternion::Identity)
+        , m_scale(Vector3::One)
+        , m_worldMatrix(Matrix::Identity)
+        , m_rotationMatrix(Matrix::Identity)
+        , m_parent(nullptr)
     {
     }
 
+
     Transform::~Transform()
+    {
+    }
+
+
+    void Transform::UpdateTransform()
+    {
+    }
+
+
+    void Transform::UpdateWorldMatrix()
     {
     }
 }
--- git_hirota2/Game/Source/Transform/Transform.h
+++ git_hirota2/Game/Source/Transform/Transform.h
@@ -1,18 +1,53 @@
-﻿#pragma once
-namespace nsEngine {
+﻿/**
+ * トランスフォームクラス
+ */
+#pragma once
+
+
+namespace nsApp {
     class Transform
     {
+    private:
+        /** 自信のパラメータ */
+        Vector3	m_localPosition;
+        Quaternion	m_localRotation;
+        Vector3 m_localScale;
+
+        /** 親トランスフォームを考慮したパラメータ */
+        Vector3	m_position;
+        Quaternion	m_rotation;
+        Vector3 m_scale;
+
+
+    private:
+        /** ワールド座標行列 */
+        Matrix m_worldMatrix;
+        /** 回転行列 */
+        Matrix m_rotationMatrix;
+
+
+    public:
+        /** NOTE:親は球体のステージ、子は手に持つ剣のイメージ */
+        /** 親のトランスフォーム */
+        Transform* m_parent;
+        /** 子のトランスフォーム群 */
+        std::vector<Transform*> m_children;
+
     public:
         Transform();
         ~Transform();
-        void SetPosition(const Vector3& pos) {
-            m_position = pos;
-        }
-        void SetRotation(const Quaternion& m_rot) {
-            m_rotation = m_rot;
-        }
+
+    public:
+        /** 更新 */
+        void UpdateTransform();
+
+
     private:
-        Vector3		m_position = Vector3::Zero; // Position in 3D space
-        Quaternion	m_rotation = Quaternion::Identity; // Rotation in 3D space
+        /** ワールド行列更新、UpdateTransform()で使用 */
+        void UpdateWorldMatrix();
+
+
+    public:
+
     };
 }
